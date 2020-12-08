29196b49bbcac6357582ff8f01a6eca50aa656d9
ignatown <ignatow.n@mail.ru>
2020-12-08 21:40:39 +0300

    4.1.2. Глупый бот [#424481]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -5,7 +5,6 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
             rsl = "Привет, умник.";
         } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
