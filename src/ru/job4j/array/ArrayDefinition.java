f3e319fa1c929d55915c43a11b35fc83eeb011b2
ignatown <ignatow.n@mail.ru>
2020-12-10 19:38:35 +0300

    6.0.2. Заполнение массива. [#424519]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен ages: " + ages.length);
         System.out.println("Размер массива равен surnames: " + surnames.length);
         System.out.println("Размер массива равен prices: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Nikita";
+        names[1] = "Andrey";
+        names[2] = "Petr";
+        names[3] = "Stas";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
